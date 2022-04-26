e874d8faab3688aa225411925992931bdc3164b6
dxxh <1198639012@qq.com>
2022-04-26 16:50:08 +0800

    解决商铺查询的缓存穿透问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -14,8 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -44,11 +43,20 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             return Result.ok(shop);
         }
 
+        // 判断是否命中的为空值
+        if (shopJson != null) {
+            // 为空值，返回一个错误信息
+            return Result.fail("店铺信息不存在");
+        }
+
         // 4.不存在，根据id查询数据库
         Shop shop = this.getById(id);
 
         // 5.不存在，返回错误
         if (shop == null) {
+            // 将空值写入redis
+            stringRedisTemplate.opsForValue().set(cacheShopKey, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+            // 返回错误信息
             return Result.fail("店铺不存在");
         }
 
